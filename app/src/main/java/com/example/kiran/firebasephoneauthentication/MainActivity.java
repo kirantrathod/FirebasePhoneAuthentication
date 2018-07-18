82280ef15abd6adb8851676ab11e1a40f1c4698e
kirantrathod <kirantrathod.kr@gmail.com>
2018-07-18 23:31:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kiran/firebasephoneauthentication/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.kiran.firebasephoneauthentication;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextPhone, editTextCode;
+
+    FirebaseAuth mAuth;
+    ProgressDialog mprogressBar;
+    String codeSent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mprogressBar=new ProgressDialog(this);
+        editTextCode = findViewById(R.id.editTextCode);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        findViewById(R.id.buttonGetVerificationCode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendVerificationCode();
+            }
+        });
+
+
+        findViewById(R.id.buttonSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mprogressBar.setTitle("Logging In...");
+                mprogressBar.setMessage("Please wait while loading!");
+                mprogressBar.setCanceledOnTouchOutside(false);
+                mprogressBar.show();
+                verifySignInCode();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    private void verifySignInCode(){
+        String code = editTextCode.getText().toString();
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //here you can open new activity
+                            mprogressBar.dismiss();
+                            Intent intent=new Intent(MainActivity.this,welcomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                            Toast.makeText(getApplicationContext(),
+                                    "Login Successfull", Toast.LENGTH_LONG).show();
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Incorrect Verification Code ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+    private void sendVerificationCode(){
+
+        String phone = editTextPhone.getText().toString();
+
+        if(phone.isEmpty()){
+            editTextPhone.setError("Phone number is required");
+            editTextPhone.requestFocus();
+            return;
+        }
+
+        if(phone.length() < 10 ){
+            editTextPhone.setError("Please enter a valid phone");
+            editTextPhone.requestFocus();
+            return;
+        }
+
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phone,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            codeSent = s;
+        }
+    };
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.menu_signout){
+            //////
+
+            new AlertDialog.Builder(this)
+                    .setMessage("Are you sure you want to Sign out?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int id) {
+                            FirebaseAuth.getInstance().signOut();
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+            ///////
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
